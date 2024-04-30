a9bc853bd3ef9478ffb82279639480dbb7a3470f
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-04-30 17:08:42 +0300

    add data

--- src/components/ChooseUsContent/ChooseUsContent.tsx
+++ src/components/ChooseUsContent/ChooseUsContent.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import contentData from '@/data/career.json'
+import data from '@/data/common.json'
 
 const ChooseUsContent = () => {
   return (
     <div className='flex flex-col sm:mr-6 lg:mr-6'>
-      <h2 className='text-mainWhite hidden font-extralight lg:flex lg:text-4xl lg:leading-10 uppercase lg:ml-[143px] lg:mb-[45px]'>Why Us ?</h2>
+      <h2 className='text-mainWhite hidden font-extralight lg:flex lg:text-4xl lg:leading-10 uppercase lg:ml-[143px] lg:mb-[45px]'>{data.title}</h2>
       <ul className='flex flex-col gap-6 gap-y-4 mr-[90px] text-right sm:w-[230px] sm:float-left sm:gap-y-8 lg:w-full sm:mr-0 sm:mb-0'>
         {contentData.map((item, index) => (
           <li key={index} className='lg:grid lg:grid-cols-2 lg:gap-y-6 lg:gap-x-6'>
