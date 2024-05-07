68a8d7e7961287e3254ff666c06ce063508dd4d0
Tokio <innka-pinnka@ukr.net>
2024-05-07 20:36:01 +0300

    refactore code

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import 'react-toastify/dist/ReactToastify.css';
+import data from '@/data/common.json'
 
 const MainForm = () => {
   return (
     <div className='flex flex-col w-[290px]'>
-      <p className="ml-auto mb-9 w-[179px] paragraph sm:ml-0 sm:mb-7 sm:w-[221px] lg:w-[234px] ">Don't miss your opportunity! Fill out the form right now and join our team!
+      <p className="ml-auto mb-9 w-[179px] paragraph sm:ml-0 sm:mb-7 sm:w-[221px] lg:w-[234px] ">{data.mainFormText}
       </p>
       <form className='flex flex-col'>
         <div className='flex flex-col'>
-          <label>Full name</label>
+          <label>{data.formLabel}</label>
           <input />
         </div>
       </form>
